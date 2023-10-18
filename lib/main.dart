e41879b4f17a6d62fe9a6fe346954b30f15f4527
Julian Sotelo <juansoos@protonmail.com>
2023-10-18 17:18:04 -0500

    feat(City Picker): create UI page (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'dart:async';
+
 import 'package:find_events/src/common/di/modules_config.dart';
 import 'package:find_events/src/config/configuration.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runZonedGuarded(() {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    final navigatorKey = FlutterModule.navigatorKey();
+    runApp(MyApp(navigatorKey: navigatorKey));
+  }, (error, stack) {
+    print('Not handled error $error, stacktrace $stack');
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.navigatorKey});
+
+  final GlobalKey<NavigatorState> navigatorKey;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Find Events',
       theme: CustomTheme.material,
       themeMode: ThemeMode.light,
---
+++ lib/src/pages/city_picker/city_picker_view_model.dart
@@ -0,0 +1,19 @@
+import 'package:find_events/src/config/configuration.dart';
+
+class CityPickerViewModel {
+  final Router _router;
+
+  CityPickerViewModel(this._router);
+
+  final List<String> cities = [
+    'New York',
+    'London',
+    'Paris',
+    'Tokyo',
+    'Sydney'
+  ];
+
+  void onCitySelected(int position) {
+    print(cities[position]);
+  }
+}
